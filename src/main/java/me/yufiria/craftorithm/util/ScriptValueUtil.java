9176abdc1fd48bd82c27235b45a20056ef1b6552
ChiyodaXiaoYi <2199098065@qq.com>
2023-03-01 20:27:50 +0800

    [1.0.0-pre4]新增papi解析表达式功能

--- src/main/java/me/yufiria/craftorithm/util/ScriptValueUtil.java
+++ src/main/java/me/yufiria/craftorithm/util/ScriptValueUtil.java
@@ -35,6 +35,7 @@ public class ScriptValueUtil {
             case "<=":
                 return var1 <= var2;
             case "=":
+            case "==":
                 return var1 == var2;
             case "!=":
                 return var1 != var2;
@@ -55,11 +56,51 @@ public class ScriptValueUtil {
             case "<=":
                 return var1 <= var2;
             case "=":
+            case "==":
                 return var1 == var2;
             case "!=":
                 return var1 != var2;
             default:
                 throw new IllegalArgumentException(type + " comparison method does not exist");
+        }
+    }
+
+    public static boolean compare(String var1, String var2, String type) {
+        Number var1Num, var2Num;
+        boolean result;
+        try {
+            if (var1.contains(".")) {
+                var1Num = Double.parseDouble(var1);
+                var2Num = Double.parseDouble(var2);
+                result = ScriptValueUtil.compare((Double) var1Num, (Double) var2Num, type);
+            } else {
+                var1Num = Integer.parseInt(var1);
+                var2Num = Integer.parseInt(var2);
+                result = ScriptValueUtil.compare((Integer) var1Num, (Integer) var2Num, type);
+            }
+            return result;
+        } catch (NumberFormatException e) {
+            switch (type) {
+                case ">":
+                    return var1.length() > var2.length();
+                case "<":
+                    return var1.length() < var2.length();
+                case ">=":
+                    return var1.length() >= var2.length();
+                case "<=":
+                    return var1.length() <= var2.length();
+                case "=":
+                case "==":
+                    return var1.equals(var2);
+                case "!=":
+                    return !var1.equals(var2);
+                case "has":
+                    return var1.contains(var2);
+                case "in":
+                    return var2.contains(var1);
+                default:
+                    throw new IllegalArgumentException(type + " comparison method does not exist");
+            }
         }
     }
 
