c09ca6bb11342b55d44ba75c4f84f460339aae3d
YufiriaMazenta <2199098065@qq.com>
2023-03-21 09:05:15 +0800

    [1.2.0-beta1]增加GUI新增切石机配方功能，完善提示消息

--- src/main/java/com/github/yufiriamazenta/craftorithm/menu/impl/recipe/RecipeCreatorMenuHolder.java
+++ src/main/java/com/github/yufiriamazenta/craftorithm/menu/impl/recipe/RecipeCreatorMenuHolder.java
@@ -9,12 +9,14 @@ import com.github.yufiriamazenta.craftorithm.recipe.RecipeFactory;
 import com.github.yufiriamazenta.craftorithm.recipe.RecipeManager;
 import com.github.yufiriamazenta.craftorithm.recipe.RecipeType;
 import com.github.yufiriamazenta.craftorithm.util.BukkitUtil;
+import com.github.yufiriamazenta.craftorithm.util.ContainerUtil;
 import com.github.yufiriamazenta.craftorithm.util.FileUtil;
 import com.github.yufiriamazenta.craftorithm.util.LangUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
@@ -62,9 +64,60 @@ public class RecipeCreatorMenuHolder extends BukkitMenuHandler {
     }
 
     private void setAnvilMenuIcons() {
+        //TODO
     }
 
     private void setStoneCuttingMenuIcons() {
+        int[] frameSlots = {
+                0, 1, 2, 3, 4, 5, 6, 7, 8,
+                9, 17,
+                18, 19, 20, 21, 23, 24, 25, 26,
+                27, 35,
+                36, 37, 38, 39, 40, 41, 42, 43, 44
+        };
+        ItemDisplayIcon frameIcon = ItemDisplayIcon.icon(Material.BLACK_STAINED_GLASS_PANE, LangUtil.lang("menu.recipe_creator.icon.frame"));
+        for (int frameSlot : frameSlots) {
+            getMenuIconMap().put(frameSlot, frameIcon);
+        }
+        ItemDisplayIcon confirmIcon = ItemDisplayIcon.icon(Material.STONECUTTER, LangUtil.lang("menu.recipe_creator.icon.frame"),
+                event -> {
+                    event.setCancelled(true);
+                    List<String> sources = new ArrayList<>();
+                    List<String> results = new ArrayList<>();
+                    for (int i = 10; i < 17; i++) {
+                        ItemStack item = event.getClickedInventory().getItem(i);
+                        if (item == null || item.getType().equals(Material.AIR))
+                            continue;
+                        String itemName = getItemName(item, true);
+                        sources.add(itemName);
+                    }
+                    for (int i = 28; i < 35; i++) {
+                        ItemStack item = event.getClickedInventory().getItem(i);
+                        if (item == null || item.getType().equals(Material.AIR))
+                            continue;
+                        String itemName = getItemName(item, false);
+                        results.add(itemName);
+                    }
+                    File recipeFile = new File(RecipeManager.getRecipeFileFolder(), recipeName + ".yml");
+                    if (!recipeFile.exists()) {
+                        FileUtil.createNewFile(recipeFile);
+                    }
+                    YamlFileWrapper recipeConfig = new YamlFileWrapper(recipeFile);
+                    recipeConfig.getConfig().set("multiple", true);
+                    recipeConfig.getConfig().set("result", results);
+                    recipeConfig.getConfig().set("source", sources);
+                    recipeConfig.getConfig().set("type", "stone_cutting");
+                    recipeConfig.saveConfig();
+                    recipeConfig.reloadConfig();
+                    Recipe[] recipes = RecipeFactory.newMultipleRecipe(recipeConfig.getConfig(), recipeName);
+                    for (Recipe recipe : recipes) {
+                        NamespacedKey key = RecipeManager.getRecipeKey(recipe);
+                        RecipeManager.regRecipe(key, recipe, recipeConfig.getConfig());
+                    }
+                    event.getWhoClicked().closeInventory();
+                    sendSuccessMsg(event.getWhoClicked());
+                });
+        getMenuIconMap().put(22, confirmIcon);
     }
 
     private void setSmithingMenuIcons() {
@@ -123,6 +176,7 @@ public class RecipeCreatorMenuHolder extends BukkitMenuHandler {
             Recipe recipe = RecipeFactory.newRecipe(recipeConfig.getConfig(), recipeName);
             RecipeManager.regRecipe(NamespacedKey.fromString(recipeName, Craftorithm.getInstance()), recipe, recipeConfig.getConfig());
             event.getWhoClicked().closeInventory();
+            sendSuccessMsg(event.getWhoClicked());
         });
         getMenuIconMap().put(22, confirmFrameIcon);
     }
@@ -224,6 +278,7 @@ public class RecipeCreatorMenuHolder extends BukkitMenuHandler {
                 RecipeManager.regRecipe(NamespacedKey.fromString(recipeName, Craftorithm.getInstance()), recipe, recipeConfig.getConfig());
             }
             event.getWhoClicked().closeInventory();
+            sendSuccessMsg(event.getWhoClicked());
         });
         getMenuIconMap().put(22, confirmIcon);
     }
@@ -335,6 +390,7 @@ public class RecipeCreatorMenuHolder extends BukkitMenuHandler {
                     Recipe recipe = RecipeFactory.newRecipe(recipeConfig.getConfig(), recipeName);
                     RecipeManager.regRecipe(NamespacedKey.fromString(recipeName, Craftorithm.getInstance()), recipe, recipeConfig.getConfig());
                     event.getWhoClicked().closeInventory();
+                    sendSuccessMsg(event.getWhoClicked());
                 });
         getMenuIconMap().put(22, confirmIcon);
     }
@@ -380,6 +436,12 @@ public class RecipeCreatorMenuHolder extends BukkitMenuHandler {
             itemName = item.getType().name();
         }
         return itemName;
+    }
+
+    private void sendSuccessMsg(HumanEntity entity) {
+        Map<String, String> replaceMap = ContainerUtil.newHashMap("<recipe_type>", recipeType.name().toLowerCase(),
+                "<recipe_name>", recipeName);
+        LangUtil.sendMsg(entity, "command.create.success", replaceMap);
     }
 
 }
