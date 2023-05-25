30e8ec2b8aba33cc68b31786194d8cc95a657211
ChiyodaXiaoYi <linjiayi357159@gmail.com>
2023-05-26 03:47:54 +0800

    [1.3.3]修复菜单中无法拿取物品的bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
 }
 
 group = "com.github.yufiriamazenta"
-version = "1.3.2"
+version = "1.3.3"
 var pluginVersion: String = version.toString() + "-" + SimpleDateFormat("yyyyMMdd").format(System.currentTimeMillis())
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
--- src/main/java/com/github/yufiriamazenta/craftorithm/menu/bukkit/BukkitMenuDispatcher.java
+++ src/main/java/com/github/yufiriamazenta/craftorithm/menu/bukkit/BukkitMenuDispatcher.java
@@ -5,13 +5,35 @@ import com.github.yufiriamazenta.craftorithm.menu.impl.recipe.RecipeDisplayMenuH
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.InventoryView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum BukkitMenuDispatcher implements Listener {
 
     INSTANCE;
+
+    private final List<InventoryAction> allowedPlayerInvActions;
+
+    BukkitMenuDispatcher() {
+        allowedPlayerInvActions = new ArrayList<>();
+        allowedPlayerInvActions.add(InventoryAction.DROP_ALL_CURSOR);
+        allowedPlayerInvActions.add(InventoryAction.DROP_ALL_SLOT);
+        allowedPlayerInvActions.add(InventoryAction.DROP_ONE_CURSOR);
+        allowedPlayerInvActions.add(InventoryAction.DROP_ONE_SLOT);
+        allowedPlayerInvActions.add(InventoryAction.NOTHING);
+        allowedPlayerInvActions.add(InventoryAction.PICKUP_ONE);
+        allowedPlayerInvActions.add(InventoryAction.PICKUP_HALF);
+        allowedPlayerInvActions.add(InventoryAction.PICKUP_ALL);
+        allowedPlayerInvActions.add(InventoryAction.PICKUP_SOME);
+        allowedPlayerInvActions.add(InventoryAction.PLACE_ONE);
+        allowedPlayerInvActions.add(InventoryAction.PLACE_ALL);
+        allowedPlayerInvActions.add(InventoryAction.PLACE_SOME);
+    }
 
     @EventHandler
     public void onClickInv(InventoryClickEvent event) {
@@ -21,7 +43,9 @@ public enum BukkitMenuDispatcher implements Listener {
         if (!(view.getTopInventory().getHolder() instanceof BukkitMenuHandler))
             return;
         if (view.getBottomInventory().equals(event.getClickedInventory())) {
-            event.setCancelled(true);
+            InventoryAction action = event.getAction();
+            if (!allowedPlayerInvActions.contains(action))
+                event.setCancelled(true);
             return;
         }
         BukkitMenuHandler handler = (BukkitMenuHandler) view.getTopInventory().getHolder();
