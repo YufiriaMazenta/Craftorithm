856cae1f5b1fbc3c199aa15d39b78ab16f0d7e96
Yufiria <xiaoyi357159@gmail.com>
2023-11-15 12:15:47 +0800

    同步一下CrypticLib的修改

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,11 +34,11 @@ dependencies {
     compileOnly("com.github.LoneDev6:API-ItemsAdder:3.5.0b")
     compileOnly("com.github.oraxen:oraxen:1.160.0")
     compileOnly("io.lumine:Mythic-Dist:5.3.5")
-    implementation("com.crypticlib:CrypticLib:0.1.3")
+    implementation("com.crypticlib:CrypticLib:0.1.4")
 }
 
 group = "com.github.yufiriamazenta"
-version = "1.4.6"
+version = "1.4.7-dev1"
 var pluginVersion: String = version.toString() + "-" + SimpleDateFormat("yyyyMMdd").format(System.currentTimeMillis())
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 java.targetCompatibility = JavaVersion.VERSION_1_8
