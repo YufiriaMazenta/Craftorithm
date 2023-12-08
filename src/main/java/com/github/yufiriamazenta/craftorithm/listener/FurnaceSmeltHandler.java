dcde5465c6f5d97b3d3681477aaef5e3861634f9
YufiriaMazenta <2199098065@qq.com>
2023-12-08 12:22:47 +0800

    [1.6.0-pre3]随机烧炼配方修改为权重算法

--- src/main/java/com/github/yufiriamazenta/craftorithm/listener/FurnaceSmeltHandler.java
+++ src/main/java/com/github/yufiriamazenta/craftorithm/listener/FurnaceSmeltHandler.java
@@ -4,7 +4,6 @@ import com.github.yufiriamazenta.craftorithm.item.ItemManager;
 import com.github.yufiriamazenta.craftorithm.recipe.RecipeGroup;
 import com.github.yufiriamazenta.craftorithm.recipe.RecipeManager;
 import com.github.yufiriamazenta.craftorithm.recipe.RecipeType;
-import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -15,19 +14,18 @@ import org.bukkit.event.inventory.FurnaceSmeltEvent;
 import org.bukkit.event.inventory.FurnaceStartSmeltEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public enum FurnaceSmeltHandler implements Listener {
 
     INSTANCE;
 
-    private final Map<Block, YamlConfiguration> furnaceMap;
+    private final Map<Block, YamlConfiguration> randomFurnaceBlockMap;
+    private Random random;
 
     FurnaceSmeltHandler() {
-        furnaceMap = new HashMap<>();
+        random = new Random();
+        randomFurnaceBlockMap = new HashMap<>();
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
@@ -47,42 +45,39 @@ public enum FurnaceSmeltHandler implements Listener {
         if (config == null)
             return;
         if (config.getString("type", "shaped").equals("random_cooking")) {
-            furnaceMap.put(event.getBlock(), config);
+            randomFurnaceBlockMap.put(event.getBlock(), config);
         }
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onFurnaceSmelt(FurnaceSmeltEvent event) {
-        if (furnaceMap.containsKey(event.getBlock())) {
-            List<String> resultList = furnaceMap.get(event.getBlock()).getStringList("result");
-            List<Map.Entry<ItemStack, Double>> probabilityList = getProbability(resultList);
-            double random = Math.random();
-            for (Map.Entry<ItemStack, Double> entry : probabilityList) {
-                if (random < entry.getValue()) {
+        if (randomFurnaceBlockMap.containsKey(event.getBlock())) {
+            List<String> resultList = randomFurnaceBlockMap.get(event.getBlock()).getStringList("result");
+            List<Map.Entry<ItemStack, Integer>> weightList = getWeight(resultList);
+            int randomNum = random.nextInt(weightList.get(weightList.size() - 1).getValue());
+            for (Map.Entry<ItemStack, Integer> entry : weightList) {
+                if (randomNum < entry.getValue()) {
                     event.setResult(entry.getKey());
                     break;
                 }
             }
-            furnaceMap.remove(event.getBlock());
+            randomFurnaceBlockMap.remove(event.getBlock());
         }
     }
 
-    private List<Map.Entry<ItemStack, Double>> getProbability(List<String> resultStr) {
-        Map<ItemStack, Double> probabilityMap = new HashMap<>();
-        double sum = 0;
+    private List<Map.Entry<ItemStack, Integer>> getWeight(List<String> resultStr) {
+        Map<ItemStack, Integer> weightMap = new HashMap<>();
+        int sum = 0;
         for (String result : resultStr) {
             String item = result.substring(0, result.lastIndexOf(" "));
-            double probability = Double.parseDouble(result.substring(result.lastIndexOf(" ") + 1));
+            int weight = Integer.parseInt(result.substring(result.lastIndexOf(" ") + 1));
             ItemStack itemStack = ItemManager.INSTANCE.matchItem(item);
-            sum += probability;
-            probabilityMap.put(itemStack, sum);
+            sum += weight;
+            weightMap.put(itemStack, sum);
         }
-        if (sum < 1.0) {
-            probabilityMap.put(new ItemStack(Material.AIR), 1.0);
-        }
-        List<Map.Entry<ItemStack, Double>> probabilityList = new ArrayList<>(probabilityMap.entrySet());
-        probabilityList.sort(Map.Entry.comparingByValue());
-        return probabilityList;
+        List<Map.Entry<ItemStack, Integer>> weightList = new ArrayList<>(weightMap.entrySet());
+        weightList.sort(Map.Entry.comparingByValue());
+        return weightList;
     }
 
 }
