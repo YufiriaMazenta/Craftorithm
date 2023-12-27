38fefe747d4aa1d4222aea070099c8a4415af701
YufiriaMazenta <2199098065@qq.com>
2023-12-27 10:47:41 +0800

    [1.7.5-dev10]修复item相关命令缺少参数提示、tab提示错误的bug

--- src/main/java/com/github/yufiriamazenta/craftorithm/cmd/sub/item/ItemCommand.java
+++ src/main/java/com/github/yufiriamazenta/craftorithm/cmd/sub/item/ItemCommand.java
@@ -1,8 +1,12 @@
 package com.github.yufiriamazenta.craftorithm.cmd.sub.item;
 
 import com.github.yufiriamazenta.craftorithm.cmd.sub.AbstractSubCommand;
+import crypticlib.command.SubcmdExecutor;
 import org.bukkit.command.CommandSender;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public final class ItemCommand extends AbstractSubCommand {
@@ -16,10 +20,11 @@ public final class ItemCommand extends AbstractSubCommand {
 
     @Override
     public boolean onCommand(CommandSender sender, List<String> args) {
-        if (args.size() < 2) {
-            sendNotEnoughCmdParamMsg(sender, 2 - args.size());
+        if (args.isEmpty()) {
+            sendNotEnoughCmdParamMsg(sender, 2);
             return true;
         }
         return super.onCommand(sender, args);
     }
+
 }
