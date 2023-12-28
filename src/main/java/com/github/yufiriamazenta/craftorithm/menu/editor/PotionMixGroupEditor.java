b6d1620b4402ea3a566d9e665809c02298417296
YufiriaMazenta <2199098065@qq.com>
2023-12-28 19:26:32 +0800

    [1.8.0-beta1]配方编辑页面初始版本基本完成

--- src/main/java/com/github/yufiriamazenta/craftorithm/menu/editor/PotionMixGroupEditor.java
+++ src/main/java/com/github/yufiriamazenta/craftorithm/menu/editor/PotionMixGroupEditor.java
@@ -1,11 +1,16 @@
 package com.github.yufiriamazenta.craftorithm.menu.editor;
 
 import com.github.yufiriamazenta.craftorithm.recipe.RecipeGroup;
+import crypticlib.ui.display.Icon;
 import crypticlib.ui.display.MenuDisplay;
 import crypticlib.ui.display.MenuLayout;
 import crypticlib.ui.menu.Menu;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PotionMixGroupEditor extends RecipeGroupEditor {
 
@@ -15,7 +20,14 @@ public class PotionMixGroupEditor extends RecipeGroupEditor {
             new MenuDisplay(
                 title,
                 new MenuLayout(
-                    //TODO
+                    Collections.singletonList(
+                        "ABCDEFGHI"
+                    ),
+                    () -> {
+                        Map<Character, Icon> iconMap = new HashMap<>();
+                        iconMap.put('A', getSortIdEditIcon(0));
+                        return iconMap;
+                    }
                 )
             )
         );
