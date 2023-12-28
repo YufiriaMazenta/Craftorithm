fa663577c56c7ff1cbe9e23752e8af3d173397dc
YufiriaMazenta <2199098065@qq.com>
2023-12-28 19:26:32 +0800

    [1.8.0-beta1]配方编辑页面初始版本基本完成

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 import java.text.SimpleDateFormat
-version = "1.7.5-dev13"
+version = "1.8.0-beta1"
 
 plugins {
     `java-library`
@@ -40,7 +40,7 @@ dependencies {
     compileOnly("io.lumine:Mythic-Dist:5.3.5")
     compileOnly("io.lumine:MythicLib-dist:1.6.2-SNAPSHOT")
     compileOnly("net.Indyuce:MMOItems-API:6.9.5-SNAPSHOT")
-    implementation("com.crypticlib:CrypticLib:0.10.21")
+    implementation("com.crypticlib:CrypticLib:0.10.22")
 }
 
 group = "com.github.yufiriamazenta"
--- src/main/java/com/github/yufiriamazenta/craftorithm/menu/editor/SmithingRecipeGroupEditor.java
+++ src/main/java/com/github/yufiriamazenta/craftorithm/menu/editor/SmithingRecipeGroupEditor.java
@@ -1,11 +1,16 @@
 package com.github.yufiriamazenta.craftorithm.menu.editor;
 
 import com.github.yufiriamazenta.craftorithm.recipe.RecipeGroup;
+import crypticlib.ui.display.Icon;
 import crypticlib.ui.display.MenuDisplay;
 import crypticlib.ui.display.MenuLayout;
 import crypticlib.ui.menu.Menu;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SmithingRecipeGroupEditor extends UnlockableRecipeGroupEditor {
 
@@ -15,7 +20,15 @@ public class SmithingRecipeGroupEditor extends UnlockableRecipeGroupEditor {
             new MenuDisplay(
                 title,
                 new MenuLayout(
-                    //TODO
+                    Collections.singletonList(
+                        "ABCDEFGHI"
+                    ),
+                    () -> {
+                        Map<Character, Icon> iconMap = new HashMap<>();
+                        iconMap.put('A', getSortIdEditIcon(0));
+                        iconMap.put('B', getUnlockIcon());
+                        return iconMap;
+                    }
                 )
             )
         );
