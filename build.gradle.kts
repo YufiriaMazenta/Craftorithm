091e99ec4a0ca6a881b1bd600f836178ff29d73c
YufiriaMazenta <2199098065@qq.com>
2024-02-05 09:33:43 +0800

    [2.0.0-dev19]重更新CrypticLib版本0.15.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 import java.text.SimpleDateFormat
-version = "2.0.0-dev18"
+version = "2.0.0-dev19"
 
 plugins {
     `java-library`
@@ -40,7 +40,7 @@ dependencies {
     compileOnly("io.lumine:Mythic-Dist:5.3.5")
     compileOnly("io.lumine:MythicLib-dist:1.6.2-SNAPSHOT")
     compileOnly("net.Indyuce:MMOItems-API:6.9.5-SNAPSHOT")
-    implementation("com.crypticlib:CrypticLib:0.15.0")
+    implementation("com.crypticlib:CrypticLib:0.15.1")
 }
 
 group = "com.github.yufiriamazenta"
