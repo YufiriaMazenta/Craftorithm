b05758877af9e391364e04ac3add282c14938795
YufiriaMazenta <linjiayi357159@gmail.com>
2024-05-29 20:50:53 +0800

    修复版本获取错误

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     compileOnly("io.lumine:MythicLib-dist:1.6.2-SNAPSHOT")
     compileOnly("net.Indyuce:MMOItems-API:6.9.5-SNAPSHOT")
     compileOnly("com.willfp:EcoEnchants:12.5.1")
-    implementation("com.crypticlib:CrypticLib:0.17.6")
+    implementation("com.crypticlib:CrypticLib:0.17.8")
 }
 
 group = "com.github.yufiriamazenta"
