d27dded1039fcdbdeede71a02b4336b3dc9a2ebd
Yufiria <xiaoyi357159@gmail.com>
2024-11-19 16:44:11 +0800

    重写RecipeManager部分底层内容

--- src/main/java/com/github/yufiriamazenta/craftorithm/recipe/ConfigRecipeLoader.java
+++
@@ -1,7 +0,0 @@
-package com.github.yufiriamazenta.craftorithm.recipe;
-
-import org.bukkit.configuration.ConfigurationSection;
-
-public interface ConfigRecipeLoader<R> extends RecipeLoader<R, ConfigurationSection> {
-
-}
