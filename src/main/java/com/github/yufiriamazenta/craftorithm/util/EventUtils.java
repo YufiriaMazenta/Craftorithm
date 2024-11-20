eb17a57ae5b59149935e2f63cadbed2e14e4de3a
Yufiria <xiaoyi357159@gmail.com>
2024-11-20 10:38:02 +0800

    不再重新注册其他事件,只重新注册几个配方相关的事件

--- src/main/java/com/github/yufiriamazenta/craftorithm/util/EventUtils.java
+++ src/main/java/com/github/yufiriamazenta/craftorithm/util/EventUtils.java
@@ -4,7 +4,7 @@ import com.github.yufiriamazenta.craftorithm.config.PluginConfigs;
 import com.github.yufiriamazenta.craftorithm.recipe.RecipeManager;
 import com.github.yufiriamazenta.craftorithm.recipe.custom.AnvilRecipe;
 import crypticlib.CrypticLibBukkit;
-import crypticlib.platform.Platform;
+import crypticlib.MinecraftVersion;
 import crypticlib.util.ItemHelper;
 import org.bukkit.NamespacedKey;
 import org.bukkit.event.Event;
@@ -16,30 +16,33 @@ import org.bukkit.inventory.Recipe;
 
 public class EventUtils {
 
-    private static Boolean hasCrafterEvent;
+    public static final Boolean hasCrafterCraftEvent;
 
     static {
+        hasCrafterCraftEvent = getHasCrafterCraftEvent();
+    }
+
+    private static Boolean getHasCrafterCraftEvent() {
         try {
             Class.forName("org.bukkit.event.block.CrafterCraftEvent");
-            hasCrafterEvent = true;
+            return true;
         } catch (ClassNotFoundException e) {
-            hasCrafterEvent = false;
+            return false;
         }
     }
 
     public static boolean isCraftorithmRecipeEvent(Event event) {
-        Recipe recipe;
+        Recipe recipe = null;
         switch (event) {
             case CraftItemEvent craftItemEvent -> recipe = craftItemEvent.getRecipe();
             case PrepareItemCraftEvent prepareItemCraftEvent -> recipe = prepareItemCraftEvent.getRecipe();
             case PrepareSmithingEvent prepareSmithingEvent -> recipe = prepareSmithingEvent.getInventory().getRecipe();
             case SmithItemEvent smithItemEvent -> recipe = smithItemEvent.getInventory().getRecipe();
-            case FurnaceStartSmeltEvent furnaceStartSmeltEvent -> recipe = furnaceStartSmeltEvent.getRecipe();
             case FurnaceSmeltEvent furnaceSmeltEvent -> recipe = furnaceSmeltEvent.getRecipe();
             case BlockCookEvent blockCookEvent -> recipe = blockCookEvent.getRecipe();
             case PrepareAnvilEvent prepareAnvilEvent -> {
                 //因为只有paper及下游服务端才有这个问题,如果识别到是bukkit或者spigot,就不用处理
-                if (CrypticLibBukkit.platform().type().equals(Platform.PlatformType.BUKKIT)) {
+                if (!CrypticLibBukkit.isPaper()) {
                     return false;
                 }
                 if (!PluginConfigs.ENABLE_ANVIL_RECIPE.value())
@@ -52,17 +55,24 @@ public class EventUtils {
                 AnvilRecipe anvilRecipe = RecipeManager.INSTANCE.matchAnvilRecipe(base, addition);
                 return anvilRecipe != null;
             }
-            case null, default -> {
-                if (hasCrafterEvent) {
+            case null -> {
+                return false;
+            }
+            default -> {
+                if (MinecraftVersion.current().afterOrEquals(MinecraftVersion.V1_17_1)) {
+                    if (event instanceof FurnaceStartSmeltEvent furnaceStartSmeltEvent) {
+                        recipe = furnaceStartSmeltEvent.getRecipe();
+                    }
+                }
+                if (hasCrafterCraftEvent) {
                     if (event instanceof CrafterCraftEvent crafterCraftEvent) {
                         recipe = crafterCraftEvent.getRecipe();
-                    } else {
-                        return false;
                     }
-                } else {
-                    return false;
                 }
             }
+        }
+        if (recipe == null) {
+            return false;
         }
         NamespacedKey recipeKey = RecipeManager.INSTANCE.getRecipeKey(recipe);
         return recipeKey != null && recipeKey.getNamespace().equals(RecipeManager.INSTANCE.PLUGIN_RECIPE_NAMESPACE);
