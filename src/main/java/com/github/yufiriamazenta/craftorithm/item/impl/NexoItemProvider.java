51c90ca33425cb754097b73f55255e4a0802c2e9
Yufiria <xiaoyi357159@gmail.com>
2025-01-07 20:46:49 +0800

    Nexo挂钩基本完成,等待测试

--- src/main/java/com/github/yufiriamazenta/craftorithm/item/impl/NexoItemProvider.java
+++ src/main/java/com/github/yufiriamazenta/craftorithm/item/impl/NexoItemProvider.java
@@ -1,8 +1,12 @@
 package com.github.yufiriamazenta.craftorithm.item.impl;
 
 import com.github.yufiriamazenta.craftorithm.item.ItemProvider;
+import com.nexomc.nexo.api.NexoItems;
+import com.nexomc.nexo.items.ItemBuilder;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Objects;
 
 public enum NexoItemProvider implements ItemProvider {
 
@@ -15,17 +19,31 @@ public enum NexoItemProvider implements ItemProvider {
 
     @Override
     public String getItemName(ItemStack itemStack, boolean ignoreAmount) {
-        return "";
+        if (!NexoItems.exists(itemStack))
+            return null;
+        String itemId = NexoItems.idFromItem(itemStack);
+        if (itemId == null) {
+            return null;
+        }
+        if (ignoreAmount) {
+            return itemId;
+        } else {
+            return itemId + " " + itemStack.getAmount() / Objects.requireNonNull(getItem(itemId)).getAmount();
+        }
     }
 
     @Override
     public ItemStack getItem(String itemName) {
-        return null;
+        ItemBuilder itemBuilder = NexoItems.itemFromId(itemName);
+        if (itemBuilder == null) {
+            return null;
+        }
+        return itemBuilder.build();
     }
 
     @Override
     public ItemStack getItem(String itemName, OfflinePlayer player) {
-        return null;
+        return getItem(itemName);
     }
 
 }
