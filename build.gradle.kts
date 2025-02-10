991370c35dbca604d014eda11c7c7faeaf58d27e
YufiriaMazenta <2199098065@qq.com>
2025-02-10 20:35:46 +0800

    更新CrypticLib仓库链接

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,7 @@ plugins {
 
 repositories {
     mavenLocal()
-    maven("http://repo.crypticlib.com:8081/repository/maven-public/") {
-        isAllowInsecureProtocol = true
-    }
+    maven("https://repo.crypticlib.com:8081/repository/maven-public/")
     maven("https://repo.auxilor.io/repository/maven-public/")
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://oss.sonatype.org/content/groups/public/")
