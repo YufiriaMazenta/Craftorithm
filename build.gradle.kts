6a8611fd97a42f47e7505a94e10efcb0197c110c
Yufiria <xiaoyi357159@gmail.com>
2025-03-22 12:04:21 +0800

    修复构建失败的问题

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,11 +30,11 @@ repositories {
 
 dependencies {
     compileOnly("org.jetbrains:annotations:24.0.1")
+//    compileOnly("org.spigotmc:spigot-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("pers.neige.neigeitems:NeigeItems:1.15.113")
     compileOnly("net.kyori:adventure-api:4.14.0")
-    compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT")
-    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
     compileOnly("org.black_ixx:playerpoints:3.2.5")
     compileOnly("net.luckperms:api:5.4")
     compileOnly("me.clip:placeholderapi:2.11.6")
