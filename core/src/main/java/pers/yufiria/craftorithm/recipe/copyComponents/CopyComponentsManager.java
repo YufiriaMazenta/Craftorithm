59e5baef405bbd1d17568bc1435273028afa13fb
Yufiria <xiaoyi357159@gmail.com>
2026-07-26 21:45:17 +0800

    refactor: 将无参数的组件保留规则折叠为枚举表
    18个单例规则类合并为SimpleCopyComponentsRules枚举, 每个条目携带
    规则名/最低版本/处理逻辑, CopyComponentsManager改为遍历注册;
    带构造参数的custom_persistent_data保持独立实现

--- core/src/main/java/pers/yufiria/craftorithm/recipe/copyComponents/CopyComponentsManager.java
+++ core/src/main/java/pers/yufiria/craftorithm/recipe/copyComponents/CopyComponentsManager.java
@@ -1,6 +1,5 @@
 package pers.yufiria.craftorithm.recipe.copyComponents;
 
-import crypticlib.MinecraftVersion;
 import crypticlib.util.IOHelper;
 import org.bukkit.NamespacedKey;
 import pers.yufiria.craftorithm.recipe.copyComponents.impl.*;
@@ -19,33 +18,12 @@ public enum CopyComponentsManager {
     private final Map<NamespacedKey, CopyComponentsRules> recipeCopyNbtRules = new ConcurrentHashMap<>();
 
     CopyComponentsManager() {
-        registerCopyNbtRuleCreator(All.INSTANCE.ruleName(), arg -> All.INSTANCE);
-        registerCopyNbtRuleCreator(Attributes.INSTANCE.ruleName(), arg -> Attributes.INSTANCE);
-        registerCopyNbtRuleCreator(CustomModelData.INSTANCE.ruleName(), arg -> CustomModelData.INSTANCE);
-        registerCopyNbtRuleCreator(DisplayName.INSTANCE.ruleName(), arg -> DisplayName.INSTANCE);
-        registerCopyNbtRuleCreator(Enchantments.INSTANCE.ruleName(), arg -> Enchantments.INSTANCE);
-        registerCopyNbtRuleCreator(ItemFlag.INSTANCE.ruleName(), arg -> ItemFlag.INSTANCE);
-        registerCopyNbtRuleCreator(Lore.INSTANCE.ruleName(), arg -> Lore.INSTANCE);
-        registerCopyNbtRuleCreator(Unbreakable.INSTANCE.ruleName(), arg -> Unbreakable.INSTANCE);
-        registerCopyNbtRuleCreator(Trim.INSTANCE.ruleName(), arg -> Trim.INSTANCE);
+        for (SimpleCopyComponentsRules rule : SimpleCopyComponentsRules.values()) {
+            if (rule.supportedByCurrentVersion()) {
+                registerCopyNbtRuleCreator(rule.ruleName(), arg -> rule);
+            }
+        }
         registerCopyNbtRuleCreator(CustomPersistentData.RULE_NAME, CustomPersistentData::new);
-
-        MinecraftVersion currentVersion = MinecraftVersion.CURRENT;
-        if (currentVersion.afterOrEquals(MinecraftVersion.V1_20_5)) {
-            registerCopyNbtRuleCreator(Food.INSTANCE.ruleName(), arg -> Food.INSTANCE);
-            registerCopyNbtRuleCreator(MaxStackSize.INSTANCE.ruleName(), arg -> MaxStackSize.INSTANCE);
-            registerCopyNbtRuleCreator(Rarity.INSTANCE.ruleName(), arg -> Rarity.INSTANCE);
-            registerCopyNbtRuleCreator(FireResistance.INSTANCE.ruleName(), arg -> FireResistance.INSTANCE);
-            registerCopyNbtRuleCreator(HideTooltip.INSTANCE.ruleName(), arg -> HideTooltip.INSTANCE);
-            registerCopyNbtRuleCreator(ItemName.INSTANCE.ruleName(), arg -> ItemName.INSTANCE);
-        }
-        if (currentVersion.afterOrEquals(MinecraftVersion.V1_21)) {
-            registerCopyNbtRuleCreator(Tool.INSTANCE.ruleName(), arg -> Tool.INSTANCE);
-        }
-        if (currentVersion.afterOrEquals(MinecraftVersion.V1_21_4)) {
-            registerCopyNbtRuleCreator(ItemModel.INSTANCE.ruleName(), arg -> ItemModel.INSTANCE);
-            registerCopyNbtRuleCreator(CustomModelDataComponent.INSTANCE.ruleName(), arg -> CustomModelDataComponent.INSTANCE);
-        }
     }
 
     public Optional<CopyComponentsRule> compileRule(String ruleStr) {
