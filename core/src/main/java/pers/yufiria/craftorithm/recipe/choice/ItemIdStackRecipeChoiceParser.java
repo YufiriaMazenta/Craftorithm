ca0fae782c4c56393cff9da31aa3dc30ba43411d
Yufiria <xiaoyi357159@gmail.com>
2026-08-22 16:18:29 +0800

    refactor(recipe): 将铁砧配方使用的RecipeChoiceParser独立出来为ItemIdStackRecipeChoiceParser

---
+++ core/src/main/java/pers/yufiria/craftorithm/recipe/choice/ItemIdStackRecipeChoiceParser.java
@@ -0,0 +1,94 @@
+package pers.yufiria.craftorithm.recipe.choice;
+
+import crypticlib.util.MaterialHelper;
+import org.bukkit.Material;
+import org.bukkit.Tag;
+import org.bukkit.inventory.RecipeChoice;
+import pers.yufiria.craftorithm.item.ItemManager;
+import pers.yufiria.craftorithm.item.ItemPack;
+import pers.yufiria.craftorithm.item.NamespacedItemId;
+import pers.yufiria.craftorithm.item.NamespacedItemIdStack;
+import pers.yufiria.craftorithm.recipe.exception.RecipeLoadException;
+import pers.yufiria.craftorithm.util.RecipeUtils;
+
+import java.util.List;
+import java.util.Optional;
+
+public enum ItemIdStackRecipeChoiceParser implements RecipeChoiceParser {
+
+    INSTANCE;
+
+    @Override
+    public RecipeChoice parse(String choiceStr) {
+        if (choiceStr == null || choiceStr.isEmpty()) {
+            throw new RecipeLoadException(choiceStr + " is not a valid ingredient ID.");
+        }
+        List<NamespacedItemIdStack> choices;
+        if (!choiceStr.contains(":")) {
+            Material material;
+            if (choiceStr.contains(" ")) {
+                //如果包含数量，需要去除数量
+                material = MaterialHelper.matchMaterial(choiceStr.substring(0, choiceStr.indexOf(" ")));
+            } else {
+                material = MaterialHelper.matchMaterial(choiceStr);
+            }
+            if (material == null) {
+                throw new RecipeLoadException(choiceStr + " is not a valid material");
+            }
+            choices = List.of(NamespacedItemIdStack.fromString(choiceStr));
+            return new StackableItemIdChoice(choices);
+        }
+        int index = choiceStr.indexOf(":");
+        String namespace = choiceStr.substring(0, index);
+        namespace = namespace.toLowerCase();
+        switch (namespace) {
+            case "minecraft":
+                Material material;
+                if (choiceStr.contains(" ")) {
+                    //如果包含数量，需要去除数量
+                    material = MaterialHelper.matchMaterial(choiceStr.substring(0, choiceStr.indexOf(" ")));
+                } else {
+                    material = MaterialHelper.matchMaterial(choiceStr);
+                }
+                if (material == null) {
+                    throw new RecipeLoadException(choiceStr + " is not a valid material");
+                }
+                choices = List.of(NamespacedItemIdStack.fromString(choiceStr));
+                break;
+            case "tag":
+                String tagKeyStr = choiceStr.substring(4);
+                int spaceIndex = tagKeyStr.lastIndexOf(' ');
+                if (spaceIndex == -1) {
+                    Optional<Tag<Material>> tagOpt = RecipeUtils.getTag(tagKeyStr);
+                    if (tagOpt.isEmpty()) {
+                        throw new RecipeLoadException(tagKeyStr + " is not a valid tag");
+                    }
+                    Tag<Material> materialTag = tagOpt.get();
+                    choices = materialTag.getValues().stream().map(it -> new NamespacedItemIdStack(NamespacedItemId.fromMaterial(it))).toList();
+                } else {
+                    int amount = Integer.parseInt(tagKeyStr.substring(spaceIndex + 1));
+                    tagKeyStr = tagKeyStr.substring(0, spaceIndex);
+                    Optional<Tag<Material>> tagOpt = RecipeUtils.getTag(tagKeyStr);
+                    if (tagOpt.isEmpty()) {
+                        throw new RecipeLoadException(tagKeyStr + " is not a valid tag");
+                    }
+                    Tag<Material> materialTag = tagOpt.get();
+                    choices = materialTag.getValues().stream().map(it -> new NamespacedItemIdStack(NamespacedItemId.fromMaterial(it), amount)).toList();
+                }
+                break;
+            case "item_pack":
+                //是物品组
+                String packId = choiceStr.substring("item_pack:".length());
+                ItemPack itemPack = ItemManager.INSTANCE.getItemPack(packId);
+                if (itemPack == null) {
+                    throw new RecipeLoadException(packId + " is not a valid item pack");
+                }
+                choices = itemPack.itemIds();
+                break;
+            default:
+                choices = List.of(NamespacedItemIdStack.fromString(choiceStr));
+                break;
+        }
+        return new StackableItemIdChoice(choices);
+    }
+}
--- core/src/main/java/pers/yufiria/craftorithm/recipe/parser/AnvilRecipeParser.java
+++ core/src/main/java/pers/yufiria/craftorithm/recipe/parser/AnvilRecipeParser.java
@@ -1,24 +1,15 @@
 package pers.yufiria.craftorithm.recipe.parser;
 
-import crypticlib.util.MaterialHelper;
-import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.Tag;
 import org.bukkit.configuration.ConfigurationSection;
 import org.jetbrains.annotations.NotNull;
 import pers.yufiria.craftorithm.Craftorithm;
-import pers.yufiria.craftorithm.item.ItemManager;
-import pers.yufiria.craftorithm.item.ItemPack;
-import pers.yufiria.craftorithm.item.NamespacedItemId;
 import pers.yufiria.craftorithm.item.NamespacedItemIdStack;
 import pers.yufiria.craftorithm.recipe.anvil.AnvilRecipe;
+import pers.yufiria.craftorithm.recipe.choice.ItemIdStackRecipeChoiceParser;
 import pers.yufiria.craftorithm.recipe.choice.RecipeChoiceParser;
 import pers.yufiria.craftorithm.recipe.choice.StackableItemIdChoice;
 import pers.yufiria.craftorithm.recipe.exception.RecipeLoadException;
-import pers.yufiria.craftorithm.util.RecipeUtils;
-
-import java.util.List;
-import java.util.Optional;
 
 public enum AnvilRecipeParser implements RecipeParser<AnvilRecipe> {
 
@@ -26,78 +17,7 @@ public enum AnvilRecipeParser implements RecipeParser<AnvilRecipe> {
 
     @Override
     public @NotNull RecipeChoiceParser choiceParser() {
-        return (choiceStr -> {
-            if (choiceStr == null || choiceStr.isEmpty()) {
-                throw new RecipeLoadException(choiceStr + " is not a valid ingredient ID.");
-            }
-            List<NamespacedItemIdStack> choices;
-            if (!choiceStr.contains(":")) {
-                Material material;
-                if (choiceStr.contains(" ")) {
-                    //如果包含数量，需要去除数量
-                    material = MaterialHelper.matchMaterial(choiceStr.substring(0, choiceStr.indexOf(" ")));
-                } else {
-                    material = MaterialHelper.matchMaterial(choiceStr);
-                }
-                if (material == null) {
-                    throw new RecipeLoadException(choiceStr + " is not a valid material");
-                }
-                choices = List.of(NamespacedItemIdStack.fromString(choiceStr));
-                return new StackableItemIdChoice(choices);
-            }
-            int index = choiceStr.indexOf(":");
-            String namespace = choiceStr.substring(0, index);
-            namespace = namespace.toLowerCase();
-            switch (namespace) {
-                case "minecraft":
-                    Material material;
-                    if (choiceStr.contains(" ")) {
-                        //如果包含数量，需要去除数量
-                        material = MaterialHelper.matchMaterial(choiceStr.substring(0, choiceStr.indexOf(" ")));
-                    } else {
-                        material = MaterialHelper.matchMaterial(choiceStr);
-                    }
-                    if (material == null) {
-                        throw new RecipeLoadException(choiceStr + " is not a valid material");
-                    }
-                    choices = List.of(NamespacedItemIdStack.fromString(choiceStr));
-                    break;
-                case "tag":
-                    String tagKeyStr = choiceStr.substring(4);
-                    int spaceIndex = tagKeyStr.lastIndexOf(' ');
-                    if (spaceIndex == -1) {
-                        Optional<Tag<Material>> tagOpt = RecipeUtils.getTag(tagKeyStr);
-                        if (tagOpt.isEmpty()) {
-                            throw new RecipeLoadException(tagKeyStr + " is not a valid tag");
-                        }
-                        Tag<Material> materialTag = tagOpt.get();
-                        choices = materialTag.getValues().stream().map(it -> new NamespacedItemIdStack(NamespacedItemId.fromMaterial(it))).toList();
-                    } else {
-                        int amount = Integer.parseInt(tagKeyStr.substring(spaceIndex + 1));
-                        tagKeyStr = tagKeyStr.substring(0, spaceIndex);
-                        Optional<Tag<Material>> tagOpt = RecipeUtils.getTag(tagKeyStr);
-                        if (tagOpt.isEmpty()) {
-                            throw new RecipeLoadException(tagKeyStr + " is not a valid tag");
-                        }
-                        Tag<Material> materialTag = tagOpt.get();
-                        choices = materialTag.getValues().stream().map(it -> new NamespacedItemIdStack(NamespacedItemId.fromMaterial(it), amount)).toList();
-                    }
-                    break;
-                case "item_pack":
-                    //是物品组
-                    String packId = choiceStr.substring("item_pack:".length());
-                    ItemPack itemPack = ItemManager.INSTANCE.getItemPack(packId);
-                    if (itemPack == null) {
-                        throw new RecipeLoadException(packId + " is not a valid item pack");
-                    }
-                    choices = itemPack.itemIds();
-                    break;
-                default:
-                    choices = List.of(NamespacedItemIdStack.fromString(choiceStr));
-                    break;
-            }
-            return new StackableItemIdChoice(choices);
-        });
+        return ItemIdStackRecipeChoiceParser.INSTANCE;
     }
 
     @Override
